55adb4c9fd6378ce7f2e8792825249afcaf20242
Simon Fay <simonjohnfay@gmail.com>
2023-11-17 16:36:06 +0000

    changed fade options and added additional parameters to stretch call

--- sjf_sola.h
+++ sjf_sola.h
@@ -16,7 +16,7 @@ public:
 //    sjf_sola(){}
 //    ~sjf_sola(){}
     
-    static inline void stretch( juce::AudioBuffer< T > &sampleToStretch, juce::AudioBuffer< T > &stretchedSample, T stretchFactor, int N = 2048, int hopSize = 256 )
+    static inline void stretch( juce::AudioBuffer< T > &sampleToStretch, juce::AudioBuffer< T > &stretchedSample, T stretchFactor, int N = 2048, int hopSize = 256, int fadeType = linear )
     {
         if ( sampleToStretch.getNumSamples() == 0 )
             return;
@@ -43,15 +43,27 @@ public:
         // slightly larger than necessary so we don't run into overruns
         stretchedSample.setSize( nChannels, Ss * M, true, true, true );
         
-        auto L = std::min( static_cast< int >( Ss / 2 ), static_cast< int >( Sa / 2 ) );
-        
+//        auto L = std::min( static_cast< int >( Ss / 2 ), static_cast< int >( Sa / 2 ) );
+        auto L = static_cast< int >( Ss / 2 );
         auto norm = 1.0 / static_cast< T >( L );
         // create fade for between sections
         std::vector< T > fade;
         fade.reserve( L );
-        for ( auto i = 0; i < L; i++ )
-//            fade.emplace_back( static_cast< T >( i ) / static_cast< T >( L ) );
-            fade.emplace_back( 0.5 - ( 0.5 * std::cos( 2.0 * M_PI * ( static_cast< T >( i ) / static_cast< T >( ( L - 1 ) * 2 ) ) ) ) );
+        switch ( fadeType ) {
+            case linear:
+                for ( auto i = 0; i < L; i++ )
+                    fade.emplace_back( static_cast< T >( i ) / static_cast< T >( L ) );
+                break;
+            case hann :
+                for ( auto i = 0; i < L; i++ )
+                    fade.emplace_back( 0.5 - ( 0.5 * std::cos( 2.0 * M_PI * ( static_cast< T >( i ) / static_cast< T >( ( L - 1 ) * 2 ) ) ) ) );
+                break;
+            default:
+                for ( auto i = 0; i < L; i++ )
+                    fade.emplace_back( static_cast< T >( i ) / static_cast< T >( L ) );
+                break;
+        }
+        
         
         // start by writing first block to stretchedBuffer
         // 0 --> Ss samples
@@ -64,10 +76,10 @@ public:
         {
             // calculate cross correlation between end of block A and beginning of block B
             //      Block A overlap start is (b-1)*Sa + Ss
-            auto blockAOverlapStart = (b-1)*Sa + Ss;
+            auto blockAIndex = (b-1)*Sa + Ss;
             //      block B start is b*Sa
-            auto blockBStart = b*Sa;
-            auto timeLag = calculateCrossCorrelationTimeLag( sampleToStretch, blockAOverlapStart, blockBStart, L, norm );
+            auto blockBIndex = b*Sa;
+            auto timeLag = calculateCrossCorrelationTimeLag( sampleToStretch, blockAIndex, blockBIndex, L, norm );
             // then do fade out of block A as block B fades in
             //      and copy to stretched sample buffer
             auto crossOverLength = timeLag + L;
@@ -77,24 +89,27 @@ public:
                 fadeIndexOut = L - fadeIndexIn;
                 fadeIn = fadeIndexIn < 0 ? 0 : fadeIndexIn < L ? fade[ fadeIndexIn ] : 1;
                 fadeOut = fadeIndexOut < 0 ? 0 : fadeIndexOut < L ? fade[ fadeIndexOut ] : 1;
-                auto segAIndex = blockAOverlapStart + s;
-                auto segBIndex = blockBStart + s;
+//                auto segAIndex = blockAOverlapStart + s;
+//                auto segBIndex = blockBStart + s;
                 for ( auto c = 0; c < nChannels; c++ )
                 {
-                    sampVal = fadeOut <= 0 ? 0 : sampleToStretch.getSample( c, segAIndex ) * fadeOut;
-                    sampVal += fadeIn <= 0 ? 0 : sampleToStretch.getSample( c, segBIndex ) * fadeIn;
+                    sampVal = fadeOut <= 0 ? 0 : sampleToStretch.getSample( c, blockAIndex ) * fadeOut;
+                    sampVal += fadeIn <= 0 ? 0 : sampleToStretch.getSample( c, blockBIndex ) * fadeIn;
                     stretchedSample.setSample( c, solaIndex, sampVal );
                 }
+                blockAIndex++;
+                blockBIndex++;
                 solaIndex++;
             }
             // then copy (Ss-crossoverLength) samples from block B to stretched sample buffer
             // block B becomes block A for next loop
             for ( auto s = 0; s < Ss - crossOverLength; s++ )
             {
-                auto segBIndex = blockBStart + timeLag + L + s;
+//                auto segBIndex = blockBStart + timeLag + L + s;
                 for ( auto c = 0; c < nChannels; c++ )
-                    stretchedSample.setSample( c, solaIndex, sampleToStretch.getSample( c, segBIndex ) );
+                    stretchedSample.setSample( c, solaIndex, sampleToStretch.getSample( c, blockBIndex ) );
                 solaIndex++;
+                blockBIndex++;
             }
         }
         // resize samples
@@ -103,6 +118,9 @@ public:
     }
     
  
+    enum fadeTypes
+    { linear, hann };
+    
 private:
     
     static inline auto calculateCrossCorrelationTimeLag( juce::AudioBuffer< T > &sampleToStretch, int segStartA, int segStartB, int L, T norm )
@@ -138,6 +156,8 @@ private:
             sum += val1[ i ] * val2[ i ];
         return sum * norm;
     }
+    
+    
 };
 
 

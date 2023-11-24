abd914f9fa544f41e76f10cfc2ab146f33752388
Simon Fay <simonjohnfay@gmail.com>
2023-11-24 16:45:28 +0000

    added cos wavetable, added amp mod to ringmod, got ssb working

--- sjf_ringMod.h
+++ sjf_ringMod.h
@@ -24,11 +24,17 @@ public:
         setModFreq( 440 );
     }
     
-    T process( T input )
+    T process( T input, int modType = modTypes::ringMod )
     {
-        T oscOut = m_osc.getValue( m_readPos, m_interpolationType );
+        m_modVal = m_osc.getValue( m_readPos, m_interpolationType );
+        m_modVal = (modType == modTypes::ampMod) ? 1 + ( m_modVal * m_ampModDepth ) : m_modVal;
         m_readPos = fastMod4< T >(m_readPos + m_readIncrementSamps, TABLE_SIZE );
-        return ( input * oscOut );
+        return ( input * m_modVal );
+    }
+    
+    T getModVal( )
+    {
+        return m_modVal;
     }
     
     void setModFreq( T f )
@@ -36,7 +42,6 @@ public:
 #ifndef NDEBUG
         assert( f > 0 );
 #endif
-//        auto periodSamps = sampleRate / f;
         m_readIncrementSamps = f * static_cast< T >( TABLE_SIZE ) / m_SR;
     }
     
@@ -44,6 +49,16 @@ public:
     {
         m_interpolationType = interpType;
     }
+    
+    void setAmpModDepth( T ampModDepthPercentage )
+    {
+#ifndef NDEBUG
+        assert( ampModDepthPercentage >= 0 && ampModDepthPercentage <= 100 );
+#endif
+        m_ampModDepth = ampModDepthPercentage* 0.01;
+    }
+    
+    enum modTypes { ringMod, ampMod };
     
 private:
     static constexpr int TABLE_SIZE = 1024;
@@ -53,6 +68,8 @@ private:
     T m_readPos = 0;
     T m_SR = 44100.0;
     T m_readIncrementSamps = static_cast< T >( TABLE_SIZE ) / m_SR;
+    T m_ampModDepth = 1;
+    T m_modVal = 0;
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR ( sjf_ringMod )
 };
 #endif /* sjf_ringMod_h */
--- sjf_wavetables.h
+++ sjf_wavetables.h
@@ -8,7 +8,9 @@
 #include <math.h>
 #include "gcem/include/gcem.hpp"
 #include "sjf_interpolators.h"
-
+//============================================================
+//============================================================
+//============================================================
 template< typename T, int TABLE_SIZE >
 struct sinArray
 {
@@ -19,6 +21,7 @@ struct sinArray
             m_table[ i ] = gcem::sin( 2.0f * M_PI * static_cast< T >(i)/static_cast< T >(TABLE_SIZE) );
         }
     }
+    ~sinArray(){}
     const T& operator[](std::size_t index) const { return m_table[ index ]; }
     const T getValue ( T findex ) const
     {
@@ -100,7 +103,201 @@ private:
     T m_table[ TABLE_SIZE ];
 };
 
+//============================================================
+//============================================================
+//============================================================
+template< typename T, int TABLE_SIZE >
+struct sjf_sinArray
+{
+    constexpr sjf_sinArray() : m_table()
+    {
+        for ( int i = 0; i < TABLE_SIZE; i++ )
+        {
+            m_table[ i ] = gcem::sin( 2.0f * M_PI * static_cast< T >(i)/static_cast< T >(TABLE_SIZE) );
+        }
+    }
+    ~sjf_sinArray(){}
+    const T& operator[](std::size_t index) const { return m_table[ index ]; }
+    const T getValue ( T findex ) const
+    {
+        T y1; // this step value
+        T y2; // next step value
+        T mu; // fractional part between step 1 & 2
+        
+        int index = findex;
+        mu = findex - index;
+        
+        fastMod3( index, TABLE_SIZE );
+        y1 = m_table[ index ];
+        fastMod3( ++index, TABLE_SIZE );
+        y2 = m_table[ index ];
+        
+        return y1 + mu*(y2-y1) ;
+    }
+    
+    const T getValue ( T findex, int interpolationType ) const
+    {
+        int pos1; // this step value
+        int pos2; // next step value
+        T mu; // fractional part between step 1 & 2
+        
+        pos1 = static_cast< int >( findex );
+        pos1 = fastMod4< int >( pos1, TABLE_SIZE );
+        pos2 = fastMod4< int >( ( pos1 + 1 ), TABLE_SIZE );
+        mu = findex - pos1;
+        switch ( interpolationType )
+        {
+            case sjf_interpolators::interpolatorTypes::linear :
+            {
+                return sjf_interpolators::linearInterpolate( mu, m_table[ pos1 ], m_table[ pos2 ] );
+                break;
+            }
+            case sjf_interpolators::interpolatorTypes::cubic :
+            {
+                auto pos0 = fastMod4< int >( pos1 - 1, static_cast< int >( TABLE_SIZE ) );
+                auto pos3 = fastMod4< int >( pos2 + 1, static_cast< int >( TABLE_SIZE ) );
+                return sjf_interpolators::cubicInterpolate( mu, m_table[ pos0 ], m_table[ pos1 ], m_table[ pos2 ], m_table[ pos3 ] );
+                break;
+            }
+            case sjf_interpolators::interpolatorTypes::pureData :
+            {
+                auto pos0 = fastMod4< int >( pos1 - 1, static_cast< int >( TABLE_SIZE ) );
+                auto pos3 = fastMod4< int >( pos2 + 1, static_cast< int >( TABLE_SIZE ) );
+                return sjf_interpolators::fourPointInterpolatePD( mu, m_table[ pos0 ], m_table[ pos1 ], m_table[ pos2 ], m_table[ pos3 ] );
+                break;
+            }
+            case sjf_interpolators::interpolatorTypes::fourthOrder :
+            {
+                auto pos0 = fastMod4< int >( pos1 - 1, static_cast< int >( TABLE_SIZE ) );
+                auto pos3 = fastMod4< int >( pos2 + 1, static_cast< int >( TABLE_SIZE ) );
+                return sjf_interpolators::fourPointFourthOrderOptimal( mu, m_table[ pos0 ], m_table[ pos1 ], m_table[ pos2 ], m_table[ pos3 ] );
+                break;
+            }
+            case sjf_interpolators::interpolatorTypes::godot :
+            {
+                auto pos0 = fastMod4< int >( pos1 - 1, static_cast< int >( TABLE_SIZE ) );
+                auto pos3 = fastMod4< int >( pos2 + 1, static_cast< int >( TABLE_SIZE ) );
+                return sjf_interpolators::cubicInterpolateGodot( mu, m_table[ pos0 ], m_table[ pos1 ], m_table[ pos2 ], m_table[ pos3 ] );
+                break;
+            }
+            case sjf_interpolators::interpolatorTypes::hermite :
+            {
+                auto pos0 = fastMod4< int >( pos1 - 1, static_cast< int >( TABLE_SIZE ) );
+                auto pos3 = fastMod4< int >( pos2 + 1, static_cast< int >( TABLE_SIZE ) );
+                return sjf_interpolators::cubicInterpolateHermite( mu, m_table[ pos0 ], m_table[ pos1 ], m_table[ pos2 ], m_table[ pos3 ] );
+                break;
+            }
+            default:
+            {
+                return sjf_interpolators::linearInterpolate( mu, m_table[ pos1 ], m_table[ pos2 ] );
+                break;
+            }
+        }
+    }
+private:
+    T m_table[ TABLE_SIZE ];
+};
 
+//============================================================
+//============================================================
+//============================================================
+
+
+template< typename T, int TABLE_SIZE >
+struct sjf_cosArray
+{
+    constexpr sjf_cosArray() : m_table()
+    {
+        for ( int i = 0; i < TABLE_SIZE; i++ )
+        {
+            m_table[ i ] = gcem::cos( 2.0f * M_PI * static_cast< T >(i)/static_cast< T >(TABLE_SIZE) );
+        }
+    }
+    ~sjf_cosArray(){}
+    const T& operator[](std::size_t index) const { return m_table[ index ]; }
+    const T getValue ( T findex ) const
+    {
+        T y1; // this step value
+        T y2; // next step value
+        T mu; // fractional part between step 1 & 2
+        
+        int index = findex;
+        mu = findex - index;
+        
+        fastMod3( index, TABLE_SIZE );
+        y1 = m_table[ index ];
+        fastMod3( ++index, TABLE_SIZE );
+        y2 = m_table[ index ];
+        
+        return y1 + mu*(y2-y1) ;
+    }
+    
+    const T getValue ( T findex, int interpolationType ) const
+    {
+        int pos1; // this step value
+        int pos2; // next step value
+        T mu; // fractional part between step 1 & 2
+        
+        pos1 = static_cast< int >( findex );
+        pos1 = fastMod4< int >( pos1, TABLE_SIZE );
+        pos2 = fastMod4< int >( ( pos1 + 1 ), TABLE_SIZE );
+        mu = findex - pos1;
+        switch ( interpolationType )
+        {
+            case sjf_interpolators::interpolatorTypes::linear :
+            {
+                return sjf_interpolators::linearInterpolate( mu, m_table[ pos1 ], m_table[ pos2 ] );
+                break;
+            }
+            case sjf_interpolators::interpolatorTypes::cubic :
+            {
+                auto pos0 = fastMod4< int >( pos1 - 1, static_cast< int >( TABLE_SIZE ) );
+                auto pos3 = fastMod4< int >( pos2 + 1, static_cast< int >( TABLE_SIZE ) );
+                return sjf_interpolators::cubicInterpolate( mu, m_table[ pos0 ], m_table[ pos1 ], m_table[ pos2 ], m_table[ pos3 ] );
+                break;
+            }
+            case sjf_interpolators::interpolatorTypes::pureData :
+            {
+                auto pos0 = fastMod4< int >( pos1 - 1, static_cast< int >( TABLE_SIZE ) );
+                auto pos3 = fastMod4< int >( pos2 + 1, static_cast< int >( TABLE_SIZE ) );
+                return sjf_interpolators::fourPointInterpolatePD( mu, m_table[ pos0 ], m_table[ pos1 ], m_table[ pos2 ], m_table[ pos3 ] );
+                break;
+            }
+            case sjf_interpolators::interpolatorTypes::fourthOrder :
+            {
+                auto pos0 = fastMod4< int >( pos1 - 1, static_cast< int >( TABLE_SIZE ) );
+                auto pos3 = fastMod4< int >( pos2 + 1, static_cast< int >( TABLE_SIZE ) );
+                return sjf_interpolators::fourPointFourthOrderOptimal( mu, m_table[ pos0 ], m_table[ pos1 ], m_table[ pos2 ], m_table[ pos3 ] );
+                break;
+            }
+            case sjf_interpolators::interpolatorTypes::godot :
+            {
+                auto pos0 = fastMod4< int >( pos1 - 1, static_cast< int >( TABLE_SIZE ) );
+                auto pos3 = fastMod4< int >( pos2 + 1, static_cast< int >( TABLE_SIZE ) );
+                return sjf_interpolators::cubicInterpolateGodot( mu, m_table[ pos0 ], m_table[ pos1 ], m_table[ pos2 ], m_table[ pos3 ] );
+                break;
+            }
+            case sjf_interpolators::interpolatorTypes::hermite :
+            {
+                auto pos0 = fastMod4< int >( pos1 - 1, static_cast< int >( TABLE_SIZE ) );
+                auto pos3 = fastMod4< int >( pos2 + 1, static_cast< int >( TABLE_SIZE ) );
+                return sjf_interpolators::cubicInterpolateHermite( mu, m_table[ pos0 ], m_table[ pos1 ], m_table[ pos2 ], m_table[ pos3 ] );
+                break;
+            }
+            default:
+            {
+                return sjf_interpolators::linearInterpolate( mu, m_table[ pos1 ], m_table[ pos2 ] );
+                break;
+            }
+        }
+    }
+private:
+    T m_table[ TABLE_SIZE ];
+};
+
+//============================================================
+//============================================================
+//============================================================
 
 template< typename T, int TABLE_SIZE >
 struct sjf_hannArray
@@ -115,7 +312,7 @@ struct sjf_hannArray
             m_table[ i + 1 ] = 0.5 - ( 0.5 * gcem::cos( 2.0 * M_PI * static_cast< T >( i ) / static_cast< T >( TABLE_SIZE - 1 ) ) );
         }
     }
-    
+    ~sjf_hannArray(){}
     const T& operator[](std::size_t index) const { return m_table[ index ]; }
     const T getValue ( T findex ) const
     {

3567f34fb14ffd0a3cf3570e5ea7321f0edcdd0f
Simon Fay <simonfay@Simon-Fay-MacBookPro-Work.local>
2026-07-15 15:49:52 +0100

    Implementing DelayLine with Pitch Shifting

--- sjf_helpers/sjf/helpers/sjf_DelayLine.h
+++ sjf_helpers/sjf/helpers/sjf_DelayLine.h
@@ -6,6 +6,8 @@
 #include <JuceHeader.h>
 #include <sjf/helpers/sjf_Interpolators.h>
 
+#include "sjf_HelperFunctions.h"
+
 namespace sjf::helpers
 {
     class DelayLine
@@ -32,6 +34,11 @@ namespace sjf::helpers
         {
             delayLine.resize(static_cast<size_t>(juce::nextPowerOfTwo(samps + 2)));
             MASK = delayLine.size() - 1;
+        }
+
+        size_t getMaxDelayTimeSamps() const
+        {
+            return delayLine.size() - 2;
         }
 
         void writeSample(const float x)
@@ -96,4 +103,105 @@ namespace sjf::helpers
 
 
     };
+}
+
+#if JUCE_MODULE_AVAILABLE_sjf_dsp
+    //====================//====================//====================//====================//====================
+    //====================//====================//====================//====================//====================
+    //====================//====================//====================//====================//====================
+    //====================//====================//====================//====================//====================
+
+#include <sjf/oscillators/sjf_Phasor.h>
+namespace sjf::helpers
+{
+    template <size_t NUM_HEADS = 3, bool EQUAL_POWER = true> // uses amplitude scaling if false
+    class PitchShiftDelayLine
+    {
+    public:
+        void prepare(const juce::dsp::ProcessSpec& spec_) noexcept
+        {
+            spec = spec_;
+            delayLine.prepare(spec);
+            phasor.prepare(spec);
+            setWindowSizeMS(windowSizeMS);
+        }
+
+        void reset() noexcept
+        {
+            phasor.reset();
+            delayLine.reset();
+        }
+
+        void setMaxDelayTimeMS(const float ms)
+        {
+            delayLine.setMaxDelayTimeMS(ms);
+        }
+
+        void setMaxDelayTimeSamps(const int samps)
+        {
+            delayLine.setMaxDelayTimeSamps(samps);
+        }
+
+        void writeSample(const float x)
+        {
+            delayLine.writeSample(x);
+        }
+
+        void setPitchShift(const float pitchShiftInSemiTones)
+        {
+            setPlayBackRate(std::pow(2.0f, pitchShiftInSemiTones/12.0f));
+        }
+
+        void setPlayBackRate(const float rate)
+        {
+            phasor.setFrequency(-1.0f*(rate - 1.0f)*invWindowSizeS);
+        }
+
+        void setWindowSizeMS(const float windowSizeMS_)
+        {
+            windowSizeMS = windowSizeMS_;
+            invWindowSizeS = 1.0f / (windowSizeMS_ / 1000.0f);
+            windowSizeSamps = windowSizeMS_ * static_cast<float>(spec.sampleRate) * 0.001f;
+        }
+
+        template<sjf::interpolation::InterpolatorTypes InterpType>
+        [[nodiscard]] float readSample(const float delayTimeSamps)
+        {
+            jassert(roundToIntAccurate(windowSizeMS * 0.001f * spec.sampleRate + delayTimeSamps) + 2 < static_cast<int>(delayLine.getMaxDelayTimeSamps()));
+            auto sum = 0.0f;
+            auto t = phasor.process();
+
+
+            for ( auto head = 0ul; head < NUM_HEADS; ++head)
+            {
+                const auto dt = t*windowSizeSamps + delayTimeSamps + 2.0f;
+                const auto e = helpers::Functions::getHannWindow(t + 0.5f);
+                sum += delayLine.readSample<InterpType>(dt) * e;
+                t = helpers::Functions::wrapPhase(t, headOffset);
+            }
+            return sum * outputScale;
+        }
+
+
+
+    private:
+        static float calculatedOutputScaling()
+        {
+            auto sum = 0.0f;
+            for ( auto head = 0ul; head < NUM_HEADS; ++head)
+                sum += helpers::Functions::getHannWindow(static_cast<float>(head)*headOffset);
+            if (EQUAL_POWER)
+                return 1.0f / sqrt(sum);
+            return 1.0f/sum;
+        }
+
+        sjf::helpers::DelayLine delayLine;
+        sjf::dsp::oscillators::Phasor<float> phasor{};
+        juce::dsp::ProcessSpec spec{};
+
+        float windowSizeMS{20}, windowSizeSamps{}, invWindowSizeS{}, outputScale{calculatedOutputScaling()};
+        static constexpr float headOffset{1.0f/static_cast<float>(NUM_HEADS)};
+    };
+
+#endif
 }

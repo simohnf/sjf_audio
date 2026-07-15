26ed714a435533d24a53de396601b8d66bda9f49
Simon Fay <simonfay@Simon-Fay-MacBookPro-Work.local>
2026-07-15 21:07:10 +0100

    Remove template

--- sjf_dsp/sjf/oscillators/sjf_Phasor.h
+++ sjf_dsp/sjf/oscillators/sjf_Phasor.h
@@ -12,82 +12,70 @@
 
 namespace sjf::dsp::oscillators
 {
+/**
+ Simple phasor class for use with modulators and other oscillators
+ */
+
+struct Phasor
+{
+    float m_increment;
+    float m_phase = 0.0f;
+
+    Phasor(){}
+
+    void prepare(const  juce::dsp::ProcessSpec& spec_)
+    {
+        spec = spec_;
+        reset();
+    }
+
+    void reset()
+    {
+        m_phase = 0.0f;
+    }
+
+    void setFrequency( const float f )
+    {
+        setFrequency(f, static_cast<float>(spec.sampleRate));
+    }
 
     /**
-     Simple phasor class for use with modulators and other oscillators
+     sets the internal frequency the phasor runs at
      */
-    template < typename Sample >
-    struct Phasor
+    void setFrequency( const float f, const float sampleRate )
     {
-        Sample m_increment;
-        Sample m_phase = 0.0f;
+        setIncrement( f / sampleRate);
+        spec.sampleRate = sampleRate;
+    }
 
-        Phasor(){}
+    /**
+     sets the increment per sample
+     */
+    void setIncrement( const float inc )
+    {
+        jassert(inc < 1.0f && inc > -1.0f);
+        m_increment = inc >= 0.0f ? inc : 1.0f - inc;
+    }
 
-        /**
-         Constructor requires frequency and samplerate values to set initial increment
-         */
-        Phasor( Sample frequency, Sample sampleRate ) : m_increment( frequency / sampleRate )
-        { spec.sampleRate = sampleRate; }
+    /**
+     Output one sample from the phasor
+     */
+    float process()
+    {
+        float p = m_phase;
+        m_phase += m_increment;
+        m_phase = m_phase - static_cast<float>(static_cast<int>(m_phase));
+        return p;
+    }
 
-
-        ~Phasor() {}
-
-        void prepare(const  juce::dsp::ProcessSpec& spec_)
-        {
-            spec = spec_;
-            reset();
-        }
-
-        void reset()
-        {
-            m_phase = 0.0f;
-        }
-
-        void setFrequency( const Sample f )
-        {
-            setFrequency(f, static_cast<float>(spec.sampleRate));
-        }
-
-        /**
-         sets the internal frequency the phasor runs at
-         */
-        void setFrequency( const Sample f, const Sample sampleRate )
-        {
-            setIncrement( f / sampleRate);
-            spec.sampleRate = sampleRate;
-        }
-
-        /**
-         sets the increment per sample
-         */
-        void setIncrement( const Sample inc )
-        {
-            jassert(inc < 1.0f && inc > -1.0f);
-            m_increment = inc >= 0.0f ? inc : 1.0f - inc;
-        }
-
-        /**
-         Output one sample from the phasor
-         */
-        Sample process()
-        {
-            Sample p = m_phase;
-            m_phase += m_increment;
-            m_phase = m_phase - static_cast<Sample>(static_cast<int>(m_phase));
-            return p;
-        }
-
-        void skip(size_t numSamplesToSkip)
-        {
-            m_phase += m_increment*static_cast<Sample>(numSamplesToSkip);
-            m_phase = m_phase - static_cast<Sample>(static_cast<int>(m_phase));
-        }
-    private:
-        juce::dsp::ProcessSpec spec{};
-    };
-
-
+    void skip(size_t numSamplesToSkip)
+    {
+        m_phase += m_increment*static_cast<float>(numSamplesToSkip);
+        m_phase = m_phase - static_cast<float>(static_cast<int>(m_phase));
+    }
+private:
+    juce::dsp::ProcessSpec spec{};
+};
 
 }
 

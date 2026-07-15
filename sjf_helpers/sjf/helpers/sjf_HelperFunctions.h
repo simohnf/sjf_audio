9272bf2afce4f797bd7ae39c0b8364b562ba7c8e
Simon Fay <simonfay@Simon-Fay-MacBookPro-Work.local>
2026-07-15 15:49:04 +0100

    Add Helper Function

---
+++ sjf_helpers/sjf/helpers/sjf_HelperFunctions.h
@@ -0,0 +1,252 @@
+//
+// Created by Simon Fay on 15/07/2026.
+//
+#pragma once
+#include <JuceHeader.h>
+
+#include "sjf_Waveshapers.h"
+
+namespace sjf::helpers
+{
+    struct Functions
+    {
+            /**
+    * Obviously not a waveform but helpful to not have to constantly retype this...
+    */
+
+    forcedinline static float floor( const float value )
+    {
+        return static_cast<float>(static_cast<int>( value ) );
+    }
+
+    /**
+     * Wraps input phase to 0-->1. Not a waveform, but useful to avoid retyping.
+     * @param phase value must be >= 0
+     * @return phase 0-->1
+     */
+
+    forcedinline static float wrapPhase( const float phase )
+    {
+        jassert ( phase >= 0 );
+        return phase - floor(phase );
+    }
+
+    /**
+     * Wraps input phase to 0-->1. Not a waveform, but useful to avoid retyping.
+     * @param phase value must be >= 0
+     * @param shift amount to shift the phase by. must be >=0
+     * @return phase 0-->1
+     */
+
+    forcedinline static float wrapPhase( float phase, const float shift )
+    {
+        jassert ( shift >= 0 );
+        jassert ( phase >= 0 );
+        phase += shift;
+        return phase - floor(phase);
+    }
+
+    /**
+     * Basic sawtooth waveform
+     *
+     * @param phase input phase 0-->1
+     * @param startingPoint changes the relationship between phase input and output of the wave. The default 0.5 means a phase of 0 outputs 0
+     * @return sawtooth wave -1-->1
+     */
+
+    forcedinline static float getSaw( float phase, const float startingPoint = 0.5f )
+    {
+        phase += startingPoint;
+        phase -= floor( phase );
+        phase *= 2.0f;
+        phase -= 1.0f;
+        jassert( phase >= -1.0f );
+        jassert( phase <= 1.0f );
+        return phase;
+    }
+
+    /**
+     * Basic sawtooth waveform
+     *
+     * @param phase input phase 0-->1
+     * @param startingPoint changes the relationship between phase input and output of the wave. The default 0.5 means a phase of 0 outputs 0
+     * @return sawtooth wave 1-->-1
+     */
+
+    forcedinline static float getSawDescending(const float phase, const float startingPoint = 0.5 )
+    {
+        return getSaw(1.0f - phase, startingPoint);
+    }
+
+    /**
+     *  WARNING: Due to inaccuracies in FastMathApproximations this may return values that are slightly above 1 or below minus 1!!!!
+     *
+     * @param phase input phase 0-->1
+     * @return sin( 2 * pi * phase )
+     */
+
+    forcedinline static float getSin( const float phase )
+    {
+        return juce::dsp::FastMathApproximations::sin( getSaw(phase) * juce::MathConstants<float>::pi);
+    }
+
+    /**
+     *  WARNING: Due to inaccuracies in FastMathApproximations this may return values that are slightly above 1 or below minus 1!!!!
+     *
+     * @param phase input phase 0-->1
+     * @return cos( 2 * pi * phase )
+     */
+
+    forcedinline static float getCos( const float phase )
+    {
+        return juce::dsp::FastMathApproximations::cos( getSaw(phase) * juce::MathConstants<float>::pi);
+    }
+
+    /**
+     * Basic triangle waveform (no duty cycle parameter...)
+     *
+     * @param phase input phase 0-->1
+     * @return triangle wave between -1-->1
+     */
+
+    static float getTriangle( const float phase ) // no duty cycle .... need to add that!!!
+    {
+        return abs( phase - floor(phase + 0.75f)  + 0.25f ) * 4.0f- 1.0f;
+    }
+
+    /**
+     * Square waveform but with a slope between the max and min values.
+     *
+     * @param phase input phase 0-->1
+     * @param squareness the higher this value the shorter the slope, i.e. the more square the resulting waveform
+     * @return -1 or 1, with ramp between values
+     */
+
+    static float getSlopedSquare( const float phase, const float squareness )
+    {
+        jassert( squareness >= 0 );
+        const auto p = getTriangle(phase) * (1 + squareness);
+        return Waveshapers::Clippers::hard(p);
+    }
+
+    /**
+     * Square waveform but with a rounded slope between the max and min values.
+     *
+     * @param phase input phase 0-->1
+     * @param squareness the higher this value the shorter the slope, i.e. the more square the resulting waveform
+     * @return -1 or 1, with ramp between values
+     */
+
+    static float getRoundedSquare( const float phase, const float squareness )
+    {
+        return juce::dsp::FastMathApproximations::cos( 0.5f*(getSlopedSquare(phase, squareness) + 1.0f) * MathConstants<float>::pi);
+    }
+
+    /**
+     * Basic square waveform. If input phase is below 0.5 returns -1, if equal to or above 0.5 returns 1
+     *
+     * @param phase input phase 0-->1
+     * @return -1 or 1
+     */
+    forcedinline static float getSquare( const float phase )
+    {
+        return phase < 0.5 ? - 1 : 1;
+    }
+
+    /**
+     * Calculate the result of a triangle wave raised to the power of 2, but with polarity retained!
+     * @param phase input phase 0-->1
+     * @return -1 <--> 1
+     */
+
+    forcedinline static float getExponential2( const float phase )
+    {
+        const auto tri = getTriangle(phase );
+        const auto sign = std::signbit(tri) ? -1.0f : 1.0f;
+        return sign*tri*tri;
+    }
+
+    /**
+     * Calculate the result of a triangle wave raised to the power of 3
+     * @param phase input phase 0-->1
+     * @return -1 <--> 1
+     */
+
+    forcedinline static float getExponential3( const float phase )
+    {
+        const auto tri = getTriangle(phase );
+        return tri*tri*tri;
+    }
+
+    /**
+     * Calculate the result of a sawtoothwave multiplied by itself and then scaled down to between -1 --> 1
+     * @param phase input phase 0-->1
+     * @param offset offsets the phase, the default 0.25 is so that this waveform hits its peaks and troughs at the same phase input as the sin calculation...
+     * @return -1 <--> 1
+     */
+    forcedinline static float getExponentialSweep( const float phase, const float offset = 0.25f )
+    {
+        jassert( phase + offset >= 0);
+        const auto saw = getSaw( phase + offset );
+        return 2.0f*saw*saw - 1.0f;
+    }
+
+    /**
+     * Calculate the result of a sine wave, scaled to between 0-->1, multiplied by itself and then scaled down to between -1 --> 1
+     * @param phase input phase 0-->1
+     * @return -1 <--> 1
+     */
+    forcedinline static float getExponentialSweep2( const float phase )
+    {
+        const auto sin = getSin( phase ) *0.5f + 0.5f;
+        return 2.0f*sin*sin - 1.0f;
+    }
+    
+    /**
+     * A waveform that can be used to shift between a sine and triangle wave
+     * @param phase input phse 0-->1
+     * @param triangleness at three the output will be a sinewave (or there abouts), the higher this value the more triangle-like the output
+     * @return -1 <--> 1
+     */
+    forcedinline static float getRoundedTriangle( const float phase, const size_t triangleness )
+    {
+        jassert( triangleness >= 3 );
+        const auto tri = getTriangle(phase);
+        return Waveshapers::Clippers::soft( tri, triangleness, true );
+    }
+
+    /**
+     * Calculates the absolute maximum result using the fast maths approximation of sin/cos above. SHould be 1, but isn't for float!
+     * @param count the number of values to check
+     * @return the maximum value calculated
+     *
+     * see @getCos @getSin
+     */
+    forcedinline static float calculateSinCosError(const size_t count = 1000000)
+    {
+        float max = 1.0f;
+        for( auto i = 0ul; i < count; ++i )
+        {
+            const auto frac = ((static_cast<float>(i) / static_cast<float>(count)) -0.5f)* 0.001f;
+            const auto posCos = wrapPhase(1.0f + frac);
+            const auto cos = abs(getCos(posCos));
+            if( cos > max )
+                max = cos;
+
+            const auto posSin = wrapPhase(0.25f + frac);
+            const auto sin =abs( getSin(posSin));
+            if( sin > max )
+                max = sin;
+        }
+        return max;
+    }
+
+
+    forcedinline static float getHannWindow(const float phase)
+    {
+        return getCos(phase) * 0.5f + 0.5f;
+    }
+
+};
+    
+}

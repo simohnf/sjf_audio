77be1ea877193c7dc59655e5baa54bea09abf7ab
Simon Fay <simonfay@simonfakprowork.home>
2026-08-15 10:58:08 +0200

    Parameter less DCBlocker

--- sjf_helpers/sjf/helpers/sjf_DCBlock.h
+++ sjf_helpers/sjf/helpers/sjf_DCBlock.h
@@ -136,4 +136,49 @@ private:
     juce::dsp::ProcessSpec spec{};
 };
 
+template<size_t NumChannels = 2, bool FIRST_ORDER = true>
+class DCBlocker_
+{
+public:
+	void prepare (const juce::dsp::ProcessSpec& spec_)
+	{
+		jassert(spec_.numChannels == NumChannels);
+		spec = spec_;
+		for (auto& dc : dcBlocker)
+		{
+			dc.prepare(spec);
+			if  constexpr (FIRST_ORDER)
+				dc.coefficients = juce::dsp::IIR::Coefficients<float>::makeFirstOrderHighPass(spec.sampleRate, 15.0f);
+			else
+				dc.coefficients = juce::dsp::IIR::Coefficients<float>::makeHighPass(spec.sampleRate, 15.0f);
+		}
+		reset();
+	}
+
+	void reset()
+	{
+		for (auto& dc : dcBlocker)
+			dc.reset();
+	}
+
+
+	template <typename ProcessContext>
+	void process (const ProcessContext& context) noexcept
+	{
+		if (ProcessContext::usesSeparateInputAndOutputBlocks())
+			context.getOutputBlock().copyFrom(context.getInputBlock());
+		for (auto c = 0ul; c < context.getOutputBlock().getNumChannels(); ++c)
+		{
+			auto& dc = dcBlocker[c];
+			auto channelBlock = context.getOutputBlock().getSingleChannelBlock(c);
+			auto channelContext = juce::dsp::ProcessContextReplacing<float>(channelBlock);
+			dc.process(channelContext);
+		}
+	}
+
+private:
+	std::array<juce::dsp::IIR::Filter<float>, NumChannels> dcBlocker;
+	juce::dsp::ProcessSpec spec{};
+};
+
 }

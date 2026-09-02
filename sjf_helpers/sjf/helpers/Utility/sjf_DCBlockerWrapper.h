c9e3c4c4d88212cddd5c005c7d13a625c44ddfd1
Simon Fay <simonfay@Simon-Fay-MacBookPro-Work.local>
2026-09-02 15:49:47 +0100

    Adding on/off switch to DCBlocker

--- sjf_helpers/sjf/helpers/Utility/sjf_DCBlockerWrapper.h
+++ sjf_helpers/sjf/helpers/Utility/sjf_DCBlockerWrapper.h
@@ -28,18 +28,22 @@ namespace sjf::helpers{
 * @tparam Processor The target audio processor class to be wrapped. Must expose standard JUCE DSP interface methods (`prepare`, `reset`, `process`, `createParameters`).
 * @tparam FirstOrder Configures the filter order of the underlying DC blocker. Set to `true` for a 1st-order (6 dB/octave) high-pass filter, or `false` for higher-order blocking. Defaults to `true`.
 */
-template <typename Processor, bool FirstOrder = true>
+template <typename Processor, bool FirstOrder = true, bool AddOnSwitch = false>
 class DCBlockerWrapper
 {
 public:
     struct Parameters : public helpers::AudioParametersBase
     {
+    	BoolState on;
         std::unique_ptr<helpers::ParameterFactory> createParameters (const juce::String&, const juce::String&) override
         {
             if (targetFactory)
             {
                 /// we insert the parameters into the processors parameter tree,
                 /// BUT The DCBlockerWrapper::Parameters struct handles smoothing/reset etc the
+
+            	if constexpr (AddOnSwitch)
+            		createTrackedParameter(*targetFactory, on, "DC", "DC Block", true);
 
              
             }
@@ -89,9 +93,12 @@ public:
             
         juce::dsp::ProcessContextReplacing<float> contextReplacing{context.getOutputBlock()};
 
+    	parameters.checkForStateChange();
+
     	processor.process (contextReplacing);
 
-    	dcBlocker.process (contextReplacing);
+    	if (applyDCBlocker())
+			dcBlocker.process (contextReplacing);
     }
 
     //==============================================================================
@@ -141,6 +148,14 @@ public:
     	sjf::optional_calls::attachToState(processor, parentTree);
     }
 private:
+	bool applyDCBlocker()
+	{
+		if constexpr (AddOnSwitch)
+			return parameters.on.currentValue;
+		else
+			return true;
+	}
+
     juce::dsp::ProcessSpec spec{};
     Processor processor;
 	BypassWrapper<DCBlocker<2, FirstOrder>, bypass_wrapper_config::OnOff> dcBlocker;
